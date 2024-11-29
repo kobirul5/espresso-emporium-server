39367823ef602817001c09375d1529afa8c92acf
kobirul5 <kobirul05j@gmail.com>
2024-11-29 11:05:15 +0600

    added post an get oparation

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ app.use(cors());
 app.use(express.json())
 
 
-//coffeeMaster2
-//0p4kSKiPdXFXtLAx
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgvjh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// const uri = "mongodb+srv://coffeeMaster2:0p4kSKiPdXFXtLAx@cluster0.dgvjh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,12 +26,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const coffeeCollection = client.db("coffeesDb").collection("coffee")
+
+    app.get("/coffees", async(req, res)=>{
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.post("/coffees", async(req, res)=>{
+        const newCoffee=  req.body;
+        console.log(newCoffee)
+        const result = await coffeeCollection.insertOne(newCoffee)
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
